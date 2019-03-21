2a9b3f298318e41d084757f467cac4e9525d83b0
LaRaxxel <47428652+LaRaxxel@users.noreply.github.com>
2019-03-21 23:11:32 +0100

    Menu Cabifai

---
+++ menu completado.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+void main(){
+
+
+printf("			 _______    _____    ______   ______   ______  _____   ______   \n");
+printf("			|          |     |  |      |     |    |       |     |     |     \n");
+printf("			|          |_____|  |      |     |    |_____  |_____|     |     \n");
+printf("			|          |     |  |-----<      |    |       |     |     |     \n");
+printf("			|          |     |  |      |     |    |       |     |     |     \n");
+printf("			|_______   |     |  |______|  ___|___ |       |     |  ___|___  \n\n\n\n");
+
+	
+
+	float rutas[7], usuario[100];
+	int i,menu;
+	
+	system("color F1");
+	//tituylo y dibujo.
+	
+	printf("         ____________                           MENU PRINCIPAL:                  \t        ____________\n");
+	printf("        / ___   __  |                           --------------                   \t       / ___   __  | \n");
+	printf("  _____/ /___| /__| |__                 1.Crear usuario.                         \t _____/ /___| /__| |__ \n");
+	printf(" |         -    -     |                                                          \t|         -    -     | \n");
+	printf(" |____   ______   ____|                 2.Usuarios registrados y gastos.         \t|____   ______   ____| \n");
+	printf("     |__|      |__|                                                              \t    |__|      |__| \n");
+	printf("                                        3.Elija su ruta.\n\n");
+	printf("                                        4.Salir de la aplicacion.\n\n");
+	scanf("%i",&menu);
+	
+	/*	switch(menu){
+		
+		case 1: introducir su nº de tlfno.
+		nombre de usuario.
+		contraseña ******
+		numero de cuenta.
+		
+		
+		case 2: una vez guardado los usuarios en un vector se muestran con x gastos ya pagados, y mostrando un determinada ruta 
+		como ultima ruta.
+		
+		
+		case 3: se plantean varias rutas:
+		1.Barajas - Embajadores con un determinado precio.
+		...
+		.....
+		asi hasta siete rutas.
+		
+		en dos o tres de ellas ponemos dos alternativas de como llegar a traves de una matriz en la que en la pantalla muestra un camino con 
+		asteriscos con un determinado precio,y otro camino con guiones que costrara otro precio.
+		
+		case 4:desea salir de cabifai??*/
+		
+	
+
+		
+	
+
+
+
+
+
+}
+
